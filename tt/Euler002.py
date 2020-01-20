cbf81bffc8fc17ce8b499d4d9e189f8a503fe4f6
timmtanner <59708962+timmtanner@users.noreply.github.com>
2020-01-20 07:49:20 -0500

    Add files via upload

--- tt/Euler002.py
+++ tt/Euler002.py
@@ -4,13 +4,9 @@ total = 0
 prev  = 1
 cur   = 2
 
-for x in range(32):
-	if cur < 4000000:
-		#print (cur)
-		if cur%2==0:
-			total += cur
-		cur += prev
-		prev = (cur-prev)
-	#else:
-		#print ("Loop No. "+str(x))
+while cur < 4000000:
+	if cur%2==0:
+		total += cur
+	cur += prev
+	prev = (cur-prev)
 print ("Total : "+str(total))
