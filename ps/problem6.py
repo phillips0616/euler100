f6930f69236c5e2f1a7dae9aa3e887f0e615720a
Phillip Smith <psmith0616@gmail.com>
2020-01-21 19:16:47 -0500

    Added solution for problem 6

---
+++ ps/problem6.py
@@ -0,0 +1,9 @@
+#sum 1 to n is (n(n + 1))/2
+
+sum_to_100_squared = ((100 * 101) / 2) ** 2
+
+# sum of squares to n is (n(n+1)(2n+1)) / 6
+
+sum_squared_to_100 = (100 * 101 * 201) / 6
+
+print(sum_to_100_squared - sum_squared_to_100)
